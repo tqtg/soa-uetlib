119b2cf0ebdd690d08539e197b1e653ae01230ad
giaplv57 <giaplvk57@gmail.com>
2015-09-22 02:16:51 +0700

    Add ngDialog, angucomplete-alt and test dialog
    Click on a random book to test

--- server/public/javascripts/app.js
+++ server/public/javascripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('soaApp', ['ngRoute']);
+var app = angular.module('soaApp', ['ngRoute', 'ui.bootstrap', 'ngDialog', 'angucomplete-alt']);
 
 app.config(function($routeProvider) {
     $routeProvider
--- server/public/javascripts/controllers/bookCtrl.js
+++ server/public/javascripts/controllers/bookCtrl.js
@@ -1,7 +1,26 @@
-app.controller('BookCtrl', function($rootScope, $scope, soaFactory) {
+app.controller('BookCtrl', function($rootScope, $scope, soaFactory, ngDialog) {
 	$scope.categories = [];
 	$scope.books = [];
 	$scope.isEditable = [];
+	// --------------------------------------
+	$scope.clickToOpen = function (book) {
+        ngDialog.open({
+				    template: '<div id="dialog">\
+				    						<div class="row">\
+				    							<div class="col-md-3 col-md-offset-1">\
+				    								<img src="'+book.image+'">\
+				    							</div>\
+				    							<div class="col-md-7 col-md-offset-1">\
+				    								<h4>'+book.title+'</h4>\
+				    								<p>'+book.author+'</p>\
+				    							</div>\
+				    						</div>\
+				    					 </div>',
+				    plain: true,
+				    className: 'ngdialog-theme-default'
+				});
+    };
+   // -----------------------------------------------
 
 	// get categories
 	soaFactory.getCategories().then(function(data) {
