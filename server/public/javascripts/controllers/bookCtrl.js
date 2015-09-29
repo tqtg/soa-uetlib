a44e285685602670a798c4918db2072c4726f00a
giaplv57 <giaplvk57@gmail.com>
2015-09-29 23:55:53 +0700

    fix unlimited request username

--- server/public/javascripts/controllers/bookCtrl.js
+++ server/public/javascripts/controllers/bookCtrl.js
@@ -81,41 +81,23 @@ app.controller('BookCtrl', function($rootScope, $scope, $http, soaFactory, ngDia
 
 	$scope.backPage = function(){
 	}
-	// // save a book to the server
-	// $scope.save = function($event) {
-	// 	if ($event.which == 13 && $scope.bookInput) {
-	// 		soaFactory.saveBook({
-	// 			"title": $scope.bookInput
-	// 		}).then(function(data) {
-	// 			$scope.books.push(data.data);
-	// 		})
-	// 		$scope.bookInput = '';
-	// 	}
-	// }
-
-	// // update the book
-	// $scope.edit = function($event, i) {
-	// 	if ($event.which == 13 && $event.target.value.trim()) {
-	// 		var b = $scope.books[i];
-	// 		soaFactory.updateBook(b._id, {
-	// 			title: $event.target.value.trim()
-	// 		}).then(function(data) {
-	// 			if (data.data) {
-	// 				b.title = $event.target.value.trim();
-	// 				$scope.isEditable[i] = false;
-	// 			} else {
-	// 				alert('Oops something went wrong!');
-	// 			}
-	// 		})
-	// 	}
-	// }
-
-	// // delete a book
-	// $scope.delete = function(i) {
-	// 	soaFactory.deleteBook($scope.books[i]._id).then(function(data) {
-	// 		if (data.data) {
-	// 			$scope.books.splice(i, 1);
-	// 		}
-	// 	})
-	// }
+	   $scope.filteredTodos = []
+  ,$scope.currentPage = 1
+  ,$scope.numPerPage = 10
+  ,$scope.maxSize = 5;
+  
+  $scope.makeTodos = function() {
+    $scope.todos = [];
+    for (i=1;i<=1000;i++) {
+      $scope.todos.push({ text:'todo '+i, done:false});
+    }
+  };
+  $scope.makeTodos(); 
+  
+  $scope.$watch('currentPage + numPerPage', function() {
+    var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+    , end = begin + $scope.numPerPage;
+    
+    $scope.filteredTodos = $scope.todos.slice(begin, end);
+  });
 });
